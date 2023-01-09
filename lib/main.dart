d2d6b37ba97933bd9911cffc49cdc15f5b4897fe
utkarshmalik19 <utkarshmalik19@gmail.com>
2023-01-09 19:51:18 +0530

    Recording screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:princeton_hive/presentation/layouts/journey_page.dart';
 import 'package:princeton_hive/utils/route_generator.dart';
 
+late List<CameraDescription> _cameras;
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
+  _cameras = await availableCameras();
   runApp(const MyApp());
 }
 
@@ -20,5 +24,55 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       onGenerateRoute: RouteGenerator.generateRoute,
     );
+  }
+}
+
+
+// Recording screen code written here due to global variable issues
+class RecordingScreen extends StatefulWidget {
+  const RecordingScreen({super.key});
+
+  @override
+  State<RecordingScreen> createState() => _RecordingScreenState();
+}
+
+class _RecordingScreenState extends State<RecordingScreen> {
+  late CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(_cameras[0], ResolutionPreset.max);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            // Handle access errors here.
+            break;
+          default:
+            // Handle other errors here.
+            break;
+        }
+      }
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+          body: Stack(children: [
+        CameraPreview(controller),
+        Padding(
+          padding: const EdgeInsets.only(top: 600, left: 170),
+          child: FloatingActionButton(
+              onPressed: () {
+                
+              }, backgroundColor: Colors.red),
+        )
+      ]));
   }
 }
---
+++ lib/presentation/layouts/recording_screen.dart
@@ -0,0 +1,57 @@
+
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+
+
+
+// class RecordingScreen extends StatefulWidget {
+//   const RecordingScreen({super.key});
+
+//   @override
+//   State<RecordingScreen> createState() => _RecordingScreenState();
+// }
+
+// class _RecordingScreenState extends State<RecordingScreen> {
+//   late CameraController controller;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     controller = CameraController(_cameras[0], ResolutionPreset.max);
+//     controller.initialize().then((_) {
+//       if (!mounted) {
+//         return;
+//       }
+//       setState(() {});
+//     }).catchError((Object e) {
+//       if (e is CameraException) {
+//         switch (e.code) {
+//           case 'CameraAccessDenied':
+//             // Handle access errors here.
+//             break;
+//           default:
+//             // Handle other errors here.
+//             break;
+//         }
+//       }
+//     });
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//           body: Stack(children: [
+//         CameraPreview(controller),
+//         Padding(
+//           padding: const EdgeInsets.only(top: 600, left: 170),
+//           child: FloatingActionButton(
+//               onPressed: () {
+                
+//               }, backgroundColor: Colors.red),
+//         )
+//       ]));
+//   }
+// }
